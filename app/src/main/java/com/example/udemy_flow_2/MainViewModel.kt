b1d46c16f1d2c5ecf10eb4d19806cf7fedd06929
monopoly11141 <monopoly1114@vt.edu>
2023-09-20 10:19:00 +0900

    Lesson 198

--- app/src/main/java/com/example/udemy_flow_2/MainViewModel.kt
+++ app/src/main/java/com/example/udemy_flow_2/MainViewModel.kt
@@ -4,13 +4,15 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     val indexFlow = flow<Int> {
-        for(i in 1..100) {
+        for (i in 1..100) {
             emit(i)
             delay(1_000L)
         }
@@ -22,7 +24,7 @@ class MainViewModel : ViewModel() {
 
     private fun backPressureDemo() {
         val indexFlowOne = flow<Int> {
-            for(i in 1..10) {
+            for (i in 1..10) {
                 Log.i("MYTAG", "Produced : $i")
                 emit(i)
                 delay(1_000L)
@@ -31,11 +33,20 @@ class MainViewModel : ViewModel() {
 
 
         viewModelScope.launch {
-            indexFlowOne.collect {
-                delay(2_000L)
-                Log.i("MYTAG", "Consumed : $it")
-            }
+            indexFlowOne
+                .filter { i -> i % 3 == 0 }
+                .map { it ->
+                    displayCount(it)
+                }
+                .collect {
+                    Log.i("MYTAG", "Consumed : $it")
+                }
         }
+
+    }
+
+    fun displayCount(count: Int): String {
+        return "Count : $count"
     }
 
 
